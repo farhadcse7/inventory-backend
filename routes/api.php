658ca12eb10724e1008e01dabe5d62872569d2ca
farhadcse7 <fmfarhad23@gmail.com>
2025-05-16 07:50:02 +0600

    16. configure repository and system setting

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\SystemSettingController;
 
 Route::post('/login', [LoginController::class, 'login']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    /* System Setting routes */
+    Route::apiResource('/system-setting', SystemSettingController::class);
+});
